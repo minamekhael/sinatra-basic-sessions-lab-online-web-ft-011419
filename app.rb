3c68eb0be6c4bf145decbc43c27c05c77ba7edb8
Mina Mekhael <mina.mekhael@ymail.com>
2019-03-01 23:19:01 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,21 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-end
+  
+  	configure do 
+		enable :sessions
+		set :session_secret, "secret"
+	end
+
+ 	get '/' do 
+		erb :index
+	end
+
+ 	post '/checkout' do
+		@item = params[:item]
+		session[:item] = @item
+
+ 		erb :checkout
+	end
+
+ end
