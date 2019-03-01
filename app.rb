09b7cff9e7c102fba9b3f02940384df6fdd18a90
Mina Mekhael <mina.mekhael@ymail.com>
2019-03-01 23:14:44 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,8 +2,12 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   
+  configure do
+    enable :sessions unless test?
+    set :session_secret, "secret"
+  end
   
   get '/hey' do 
-  @session = session
-end
+    @session = session
+  end
 end
